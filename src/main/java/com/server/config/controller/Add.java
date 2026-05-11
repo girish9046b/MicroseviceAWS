952f2edae929926360a0fde8de692c3270d5c2c5
girish9046b <90566230+girish9046b@users.noreply.github.com>
2026-05-11 12:36:39 +0530

    spec

--- src/main/java/com/server/config/controller/Add.java
+++ src/main/java/com/server/config/controller/Add.java
@@ -21,7 +21,7 @@ public class Add {
 	//	       @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value="60")
 	//	    })
 	public int getSum(@PathVariable("a") int a,@PathVariable("b") int b) {
-		System.out.println("...........Add server-1111111111111.22233 ........... ");
+		System.out.println("...........Add server-1111111111111.2223 3 ........... ");
 		
 		int sum = a + b + 10 ;  //restTemplate.getForObject("http://CLIENT2/maths/sum/"+a+"/"+b, Integer.class);
 		return sum;
